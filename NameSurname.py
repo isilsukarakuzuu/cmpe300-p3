4260f3974ffeabb5b93bc045f7f53823b8105f7c
araldortogul <araldortogul@gmail.com>
2023-01-08 16:59:20 +0300

    Updated code

--- NameSurname.py
+++ NameSurname.py
@@ -1,10 +1,9 @@
-import random
-import time
-import sys
+import random, math, time, sys
 
-sys.setrecursionlimit(100000)
+sys.setrecursionlimit(10010)
 
-'''Ver1. The classical deterministic algorithm. The pivot is chosen as the first element of the 
+'''
+Ver1. The classical deterministic algorithm. The pivot is chosen as the first element of the 
 list.
 Ver2. The randomized algorithm. The pivot is chosen randomly. This is the algorithm 
 “Quicksort (1st version)” in the course slides.
@@ -17,19 +16,16 @@ rule.
 
 #ver_type decides the version of quick sort 
 def pivot_decider(ver_type, array, low, high):
-  if(ver_type == 1):
+  if ver_type == 1 or ver_type == 3:
     return array[low]
   if(ver_type == 2):
     return random.choice(array[low:high+1])
-  if(ver_type == 3):
-    random.shuffle(array[low:high+1])
-    return array[low]
   if(ver_type == 4):
     return median_of_three(array,low,high)
 
 #median of three function for Ver4:
 def median_of_three(array,low,high):
-  mid = low + (high - low)//2
+  mid = low + (high - low) // 2
   median = sorted([array[low], array[high], array[mid]])[1]
   return median
 
@@ -64,24 +60,16 @@ def quick_sort(ver_type, array, low, high):
 
 #generating input
 
-'''InpType1. Each element of the list is an integer between 1 and 10*n. The elements are 
+'''
+InpType1. Each element of the list is an integer between 1 and 10*n. The elements are 
 randomly chosen within this range. Note that the elements in the list will be 
 mostly distant integers due to the interval used to choose the elements.
 InpType2. Each element of the list is an integer between 1 and 0.75*n. Note that in this case 
 there will be duplicate elements in the list.
 InpType3. Each element of the list is an integer between 1 and 0.25*n. Note that in this case 
 there will be much more duplicate elements in the list.
-InpType4. All the elements are the integer 1.'''
-
-def generate_array(type, size):
-  if(type == 1):
-     return [random.randint(1, 10 * size) for i in range(size)]
-  if(type == 2):
-     return [random.randint(1, 0.75 * size) for i in range(size)]
-  if(type == 3):
-    return [random.randint(1, 0.25 * size) for i in range(size)]
-  if(type == 4):
-    return [1] * size
+InpType4. All the elements are the integer 1.
+'''
 
 def run(sort_type, n, input):
   t = time.time()
@@ -89,16 +77,45 @@ def run(sort_type, n, input):
   t = time.time() - t
   return t
 
-for sort_type in range(1,5):
-  for n in [100, 1000,10000]:
-    for input_type in range(1,5):
+f = open('output.txt', 'w')
+
+def printArray(array, size):
+  for i in range(size - 1):
+    f.write(f'{array[i]}-')
+
+  f.write(f'{array[size - 1]}\n')
+
+# Version
+for n in [100, 1000, 10000]:
+  for input_type, maxElement in enumerate([10*n, math.ceil(0.75*n), math.ceil(0.25*n), 1]):
+    for version in range(1,5):
+      # Average Case
       average_time = 0
       for average in range(1,6):
-        input = generate_array(input_type, n)
-        average_time += run(sort_type, n, input)
+        averageInput = [random.randint(1, maxElement) for T in range(n)]
+
+        # If version is 3, start the algorithm by shuffling the list
+        if version == 3:
+          random.shuffle(averageInput)
+        f.write(f'Input {average}: (Average)')
+        printArray(averageInput, n)
+        average_time += run(version, n, averageInput)
+        f.write(f'Output {average}: (Average)')
+        printArray(averageInput, n)
       average_time /= 5
-      worstTime = run(sort_type, n, sorted(generate_array(input_type, n), reverse=True))
 
-      print(f"SortType: {sort_type} n: {n} inputType: {input_type} averageTime: {average_time} worstTime: {worstTime}")
+      # Worst Case
+      worstInput = sorted([random.randint(1, maxElement) for T in range(n)])
+      f.write('Input: (Worst)')
+      printArray(worstInput, n)
 
-  
+      # If version is 3, start the algorithm by shuffling the list
+      if version == 3:
+          random.shuffle(worstInput)
+
+      worstTime = run(version, n, worstInput)
+      f.write('Output: (Worst)')
+      printArray(worstInput, n)
+
+      f.write(f"Version: {version}\tN: {n}\tinputType: {input_type + 1}\tAverage Time: {average_time}\tWorst Time: {worstTime}\n\n")
+
